15084d706edadbb8e12698f9e3d60e07b646a53d
Gannio <thegreatgannio@gmail.com>
2022-04-30 15:33:56 -0700

    Initial content commit.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GMSQuickCompile
-A tool for quickly compiling large GMS projects without the need to load said project within GMS itself.
+This is a program meant for launching GMS1.x projects without the need to launch the program within GMS itself, which causes large loading times due to how giant GMS itself is.
+
+It works by executing a process in a game, using a DLL made specifically for this purpose to do so. Apparently, because the game is connected to GMS via the Runner, it considers commands from said game valid in regards to the compiler's DRM (Even if the shell is made seperate).
+
+For the same reason, this program will *not* work if you try to compile the code to an exe, as then it's seperated from GMS. As such, there is no release build as it'd be pointless.
---
+++ meta/QuickCompile/QuickCompile/Class1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace QuickCompile
+{
+    public class Class1
+    {
+        [DllExport("QuickCompile_Compile", CallingConvention.Cdecl)]
+        public static unsafe double CompileTest(char* parametersC)
+        {
+
+            string[] parameters = Marshal.PtrToStringAnsi((IntPtr)parametersC).Replace("\\\\","\\").Split('*');
+
+
+            var t = Task.Run(() => Compile(parameters));
+            //Compile(parameters);
+            
+            //string output = p.StandardOutput.ReadToEnd();
+            //Console.WriteLine(output);
+            return 1;
+        }
+        static string lastRunCommand = "";
+        static string lastRunArgs = "";
+        static string lastRunCD = "";
+        private static void Compile(string[] parameters)
+        {
+            string cacheLoc = parameters[0];//Marshal.PtrToStringAnsi((IntPtr)detailsC);
+            string tempLoc = parameters[1];//Marshal.PtrToStringAnsi((IntPtr)stateC);
+            string compilerLoc = parameters[2];//Marshal.PtrToStringAnsi((IntPtr)largeC);
+            string projectLoc = parameters[3];// Marshal.PtrToStringAnsi((IntPtr)smallC);
+            string runnerLoc = "\"" + parameters[4] + "\"";
+            DateTime now = DateTime.Now;
+            string tempName = "\\gm_TQCFiles\\gm_TQC_" + now.Ticks.ToString();
+
+            string gameName = Path.GetFileNameWithoutExtension(projectLoc).Replace(".project","");
+
+            //string tempName = "\\gm_ttt_77777\\gm_ttt_50518";
+            //string largeImage = parameters[4];// Marshal.PtrToStringAnsi((IntPtr)largeImageC);
+            //string smallImage = parameters[5];// Marshal.PtrToStringAnsi((IntPtr)smallImageC);
+
+
+            string program = "\"" + @"C:\Program Files (x86)\Steam\steamapps\common\gamemaker_studio\GMAssetCompiler.exe" + "\"";
+            string args = " /c /m=win  /config=\"Default\" /tgt=64 /obob=True /obpp=False /obru=True /obes=False /i=3 /j=16 /cvm /tp=2048 /mv=1 /iv=0 /rv=0 /bv=1804 /gn=\"" + gameName + "\" /td=\"" + tempLoc + "\" /cd=\"" + cacheLoc + "\" /sh=True /dbgp=\"6502\" /hip=\"192.168.56.1\" /hprt=\"51268\" /o=\"" + tempLoc + tempName + "\" \"" + projectLoc + "\"";
+
+
+
+
+
+
+            var psi = new ProcessStartInfo(program)
+            {
+                Arguments = args,
+
+                UseShellExecute = true,
+                CreateNoWindow = false,
+                //RedirectStandardInput = true,
+                //RedirectStandardOutput = true
+            };
+            var p = Process.Start(psi);
+            p.WaitForExit();
+
+
+            /*string newRunner = tempLoc + tempName + "\\Runner.exe";//Because of a quirk with FMOD running on a runner ran outside GMS, we need to copy it to the local area. 4MB per compile just about.
+            Console.WriteLine(newRunner);//File.WriteAllText("test.txt", newRunner);
+            File.Copy(parameters[4],newRunner);
+
+            //File.Move(tempLoc+tempName + "\\" + gameName + ".win", tempLoc + tempName + "\\data.win");
+            
+            lastRunCommand = newRunner;
+            lastRunArgs = " -game " + tempLoc + tempName + "\\" + gameName + ".win";
+
+
+            File.WriteAllText("cd " + tempLoc + tempName + "\n" + "runLast.bat", lastRunCommand + lastRunArgs);
+            lastRunCommand = "CMD.exe";
+            lastRunArgs = "/C " + tempLoc + tempName + "\\" + "runLast.bat";
+            */
+
+
+            lastRunCommand = runnerLoc;//newRunner;
+            lastRunArgs = " -game " + tempLoc + tempName + "\\" + gameName + ".win";
+            lastRunCD = "cd \"" + tempLoc + tempName + "\\\"";
+
+            File.WriteAllText(tempLoc + tempName + "\\" + "run.bat", lastRunCD + "\n" + lastRunCommand + lastRunArgs);
+
+            lastRunCommand = tempLoc + tempName + "\\" + "run.bat";
+            lastRunArgs = "";
+            //Run();
+
+            Run();
+
+            return;
+            psi = new ProcessStartInfo(lastRunCommand)
+            {
+                Arguments = lastRunArgs,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WorkingDirectory = tempLoc + tempName + "\\"
+            };
+            Console.WriteLine(lastRunCommand);
+            //p.StartInfo = psi;
+            //p.Start();
+            Process.Start(tempLoc + tempName + "\\" + "runLast.bat", null,null,"GMSTest");
+            Console.WriteLine(program + args);
+        }
+        [DllExport("QuickCompile_Run", CallingConvention.Cdecl)]
+        public static unsafe double RunTest()
+        {
+            if (lastRunCommand == "")
+            {
+                Console.WriteLine("Wait for compile to finish first!");
+                return 0;
+            }
+            Run();
+            return 1;
+
+        }
+        private static void Run()
+        {
+            Console.WriteLine(lastRunCommand);
+
+            var psi = new ProcessStartInfo(lastRunCommand)
+            {
+                Arguments = lastRunArgs,
+                UseShellExecute = true,
+                CreateNoWindow = false,
+                //RedirectStandardInput = true,
+                //RedirectStandardOutput = true
+            };
+            var p = Process.Start(psi);
+
+            /*p.StandardInput.WriteLine(lastRunCD);
+            p.StandardInput.WriteLine(lastRunCommand + lastRunArgs);*/
+        }
+    }
+}
